14e1ba8a36bb9189e36e8cfd796378ecace7255b
SMRT <Kirill1309913099@yandex.ru>
2024-12-16 13:19:48 +0300

    correct

--- cart/task.js
+++ cart/task.js
@@ -1,46 +1,44 @@
 const controls = [...document.querySelectorAll(".product__quantity-controls")]
-const product__value = [...document.querySelectorAll(".product__quantity-value")]
-const product__add = [...document.querySelectorAll(".product__add")]
+const productValue = [...document.querySelectorAll(".product__quantity-value")]
+const productAdd = [...document.querySelectorAll(".product__add")]
 const cart = document.querySelector(".cart__products")
-const products = []
 
 controls.forEach((element, index) => {
     element.addEventListener('click', (e) => {
     if (e.target.innerText === "+") {
-        product__value[index].textContent = Number(product__value[index].textContent) + 1
+        productValue[index].textContent = Number(productValue[index].textContent) + 1
     }
-    else if (e.target.innerText === "-" & Number(product__value[index].textContent) > 1) {
-        product__value[index].textContent = Number(product__value[index].textContent) - 1
+    else if (e.target.innerText === "-" & Number(productValue[index].textContent) > 1) {
+        productValue[index].textContent = Number(productValue[index].textContent) - 1
     }
     })
 })
 
 const addCart = ((img, count, id) => {
-    if (products.includes(id)) {
-        let cart_products = [...document.querySelectorAll(".cart__product")]
-        cart_products.forEach((product) => {
-            if (product.getAttribute('data-id') === id) {
-                product.lastElementChild.textContent = Number(product.lastElementChild.textContent) + count
-            }
-        })
-    }
-    else {
-    products.push(id)
+    let cartProducts = [...document.querySelectorAll(".cart__product")]
 
-    cart.insertAdjacentHTML('afterbegin', `
-        <div class="cart__product" data-id=${id}>
-            <img class="cart__product-image" src=${img}>
-            <div class="cart__product-count">${count}</div>
-        </div>
-        `);
+    const productInCard =  cartProducts.find((element) =>
+        element.getAttribute('data-id') === id
+    )
+    if(productInCard) {
+        productInCard.lastElementChild.textContent = Number(productInCard.lastElementChild.textContent) + count
+
+      } 
+    else {
+        cart.insertAdjacentHTML('afterbegin', `
+            <div class="cart__product" data-id=${id}>
+                <img class="cart__product-image" src=${img}>
+                <div class="cart__product-count">${count}</div>
+            </div>
+            `);
     }
 })
 
-product__add.forEach((element, index) => {
+productAdd.forEach((element, index) => {
     element.addEventListener('click', (e) => {
         let product = e.target.closest(".product")
         let img = `${product.getElementsByTagName("img")[0].src}`
-        let count = Number(product__value[index].textContent)
+        let count = Number(productValue[index].textContent)
         let id = product.getAttribute("data-id")
         addCart(img, count, id)
     })
--- tooltip/task.js
+++ tooltip/task.js
@@ -1,22 +1,24 @@
-const tooltips = [...document.querySelectorAll('.has-tooltip')]
+const tooltips = [...document.querySelectorAll(".has-tooltip")]
 
-const clear = (() =>{
-    tooltips.forEach(el => {
-        el.nextElementSibling.className = "tooltip"
-    })
-});
+tooltips[0].insertAdjacentHTML('afterEnd', `
+    <div class="tooltip" style="top: 0vw; left: 0px;"></div>
+`);
+const tooltip = document.querySelector(".tooltip")
+
 
 tooltips.forEach(el => {
-    el.insertAdjacentHTML('afterEnd', `
-        <div class="tooltip" style="top: 20vw; left: 0px;">
-        ${el.title}
-        </div>
-    `);
-
     el.addEventListener('click', (e) => {
         e.preventDefault()
-        clear()
-        el.nextElementSibling.classList.toggle("tooltip_active")
+        let left = (el.getBoundingClientRect().left)
+        let top = (el.getBoundingClientRect().bottom) + 5
 
+       if (el.title === tooltip.textContent) {
+        tooltip.classList.toggle("tooltip_active")
+       }
+       else {
+        tooltip.textContent = el.title
+        tooltip.style=`top: ${top}px; left: ${left}px;`
+        tooltip.classList.add("tooltip_active")
+       }
     })
 })
